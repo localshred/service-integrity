a20d6002dd2fc0199c77a4f64d021c585f78adaf
BJ Neilsen <bj.neilsen@gmail.com>
2018-09-26 21:38:25 -0400

    Add test case validating escalating priority

--- test/state-resolver.test.ts
+++ test/state-resolver.test.ts
@@ -1,35 +1,54 @@
 /* eslint-env jest */
 
 import Bluebird = require('bluebird')
-import stateResolver = require('../src/state-resolver')
+import {
+  configureMiddleware,
+  IServiceResult,
+  IServiceState
+} from '../src/state-resolver'
 import { ServiceState } from '../src/status'
 
-const { configureMiddleware } = stateResolver
+const resolveToState = (
+  status: ServiceState,
+  message?: string
+): Bluebird<IServiceState> => Bluebird.resolve({ status, message })
 
 describe('src/state-resolver', () => {
   describe('src/configureMiddleware', () => {
     it('resolves all services and produces an overallStatus', () =>
       configureMiddleware({
         services: {
-          mysql: Bluebird.resolve({ status: 'OK' as ServiceState })
+          mysql: resolveToState('OK')
         }
-      }).then((result: stateResolver.IServiceResult): void => {
+      }).then((result: IServiceResult): void => {
         expect(result.overallStatus).toEqual('OK')
         expect(result.services.mysql).toEqual({ status: 'OK' as ServiceState })
       }))
 
-    it('returns a warning if at least one service is in the WARN state', () =>
+    it('returns an overall WARN if at least one service is in the WARN state', () =>
       configureMiddleware({
         services: {
-          elasticsearch: Bluebird.resolve({
-            status: 'WARN' as ServiceState
-          }),
-          mysql: Bluebird.resolve({ status: 'OK' as ServiceState })
+          elasticsearch: resolveToState('WARN'),
+          mysql: resolveToState('OK')
         }
-      }).then((result: stateResolver.IServiceResult): void => {
+      }).then((result: IServiceResult): void => {
         expect(result.overallStatus).toEqual('WARN')
         expect(result.services.mysql).toEqual({ status: 'OK' })
         expect(result.services.elasticsearch).toEqual({ status: 'WARN' })
+      }))
+
+    it('returns an overall ERROR if at least one service is in the ERROR state', () =>
+      configureMiddleware({
+        services: {
+          elasticsearch: resolveToState('WARN'),
+          memcached: resolveToState('ERROR'),
+          mysql: resolveToState('OK')
+        }
+      }).then((result: IServiceResult): void => {
+        expect(result.overallStatus).toEqual('ERROR')
+        expect(result.services.elasticsearch).toEqual({ status: 'WARN' })
+        expect(result.services.memcached).toEqual({ status: 'ERROR' })
+        expect(result.services.mysql).toEqual({ status: 'OK' })
       }))
   })
 })
