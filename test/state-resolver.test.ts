2625b3836184fe3822f5dc1349319fff7b927a8c
BJ Neilsen <bj.neilsen@gmail.com>
2018-09-26 21:33:03 -0400

    Use union type status as string to make it readable

--- test/state-resolver.test.ts
+++ test/state-resolver.test.ts
@@ -2,21 +2,34 @@
 
 import Bluebird = require('bluebird')
 import stateResolver = require('../src/state-resolver')
+import { ServiceState } from '../src/status'
+
+const { configureMiddleware } = stateResolver
 
 describe('src/state-resolver', () => {
   describe('src/configureMiddleware', () => {
-    it('accepts an object of key/value pairs describing services', () =>
-      stateResolver
-        .configureMiddleware({
-          services: {
-            mysql: Bluebird.resolve({ status: stateResolver.ServiceState.OK })
-          }
-        })
-        .then((result: stateResolver.IServiceResult) => {
-          expect(result.overallStatus).toEqual(stateResolver.ServiceState.OK)
-          expect(result.services.mysql).toEqual({
-            status: stateResolver.ServiceState.OK
-          })
-        }))
+    it('resolves all services and produces an overallStatus', () =>
+      configureMiddleware({
+        services: {
+          mysql: Bluebird.resolve({ status: 'OK' as ServiceState })
+        }
+      }).then((result: stateResolver.IServiceResult): void => {
+        expect(result.overallStatus).toEqual('OK')
+        expect(result.services.mysql).toEqual({ status: 'OK' as ServiceState })
+      }))
+
+    it('returns a warning if at least one service is in the WARN state', () =>
+      configureMiddleware({
+        services: {
+          elasticsearch: Bluebird.resolve({
+            status: 'WARN' as ServiceState
+          }),
+          mysql: Bluebird.resolve({ status: 'OK' as ServiceState })
+        }
+      }).then((result: stateResolver.IServiceResult): void => {
+        expect(result.overallStatus).toEqual('WARN')
+        expect(result.services.mysql).toEqual({ status: 'OK' })
+        expect(result.services.elasticsearch).toEqual({ status: 'WARN' })
+      }))
   })
 })
